023f00a7700be9a696e89bd0486b2ebe7d427bc0
Snahil Singh Dasawat <92240128+dsnahil@users.noreply.github.com>
2022-08-23 22:25:13 +0530

    Update practical1.kt

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,7 +1,13 @@
  fun main()
- {
-     var x : Int = 100
-     val y=200
-     println("x=$x")
-     println("y=$y")
- }
+{
+    var a=10
+    var b=20
+    var temp:Int
+    println("Before Swapping:")
+    println("The value of a is:$a and Value of B is:$b")
+    temp=a
+    a=b
+    b=temp
+    println("After swapping")
+    println("The value of a is:$a and Value of B is:$b")
+}
