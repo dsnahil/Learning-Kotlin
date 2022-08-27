79a53acbe9c5a7587b1f3f35df71ea57b034e80c
Snahil Singh Dasawat <92240128+dsnahil@users.noreply.github.com>
2022-08-27 13:48:03 +0530

    Practical 1

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,13 +1,21 @@
  fun main()
 {
-    var a=10
-    var b=20
-    var temp:Int
-    println("Before Swapping:")
-    println("The value of a is:$a and Value of B is:$b")
-    temp=a
-    a=b
-    b=temp
-    println("After swapping")
-    println("The value of a is:$a and Value of B is:$b")
+    var i: Int=100
+    var f: Float=3.2F
+    var c: Char='s'
+    var s: String="Hello World"
+    var b: Boolean=true
+    var d: Double=574.342
+    var l: Long=20012011162
+    var sh: Short=-2
+    var byt: Byte=16
+    println("Integer value = $i")
+    println("Float value = $f")
+    println("Character value = $c")
+    println("String value = $s")
+    println("Boolean value = $b")
+    println("Double value = $d")
+    println("Long value = $l")
+    println("Short value = $sh")
+    println("Byte value = $byt")        
 }
